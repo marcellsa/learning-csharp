376c5755fa725a805dfb741a6d926cfd094197aa
Marcel Albuquerque <msalbuquerque@hotmail.com>
2024-01-04 11:26:15 -0300

    feat(exercicios-10): instaciando 5 filmes e exibindo informações

--- Exercicios-10/Filme.cs
+++ Exercicios-10/Filme.cs
@@ -6,14 +6,41 @@ public class Filme
 {
     // Propriedades do filme
     public string Titulo { get; set; }
-    public int Durcao { get; set; }
+    public int Duracao { get; set; }
     public List<string> Elenco { get; set; }
 
     // Construtor
-    public Filme(string titulo, int duracao, List<string> elenco)
+    public Filme(string titulo, int duracao, List<string>? elenco)
     {
         Titulo = titulo;
-        Durcao = Durcao;
-        Elenco = elenco;
+        Duracao = duracao;
+        if (elenco == null)
+        {
+            Elenco = new List<string>();
+        }
+        else
+        {
+            Elenco = elenco;
+        }
+    }
+
+    // Método para exibir informações do filme
+    public void ExibirInformacoesDoFilme()
+    {
+        Console.WriteLine("\nInformações sobre o Filme:");
+        Console.WriteLine($"Título: {Titulo}");
+        Console.WriteLine($"Duração: {Duracao} minutos");
+        Console.WriteLine("Elenco:");             
+        if(Elenco.Count == 0)
+        {
+            Console.WriteLine("Elenco vazio.");
+        }
+        else
+        {
+            foreach(var artista in Elenco)
+            {
+                Console.WriteLine($"- {artista}");
+            }
+        }        
     }
 }
--- Exercicios-10/Program.cs
+++ Exercicios-10/Program.cs
@@ -3,6 +3,19 @@
 // 3.1 Criar uma classe que representa um filme, com dados como seu titulo, duração e elenco. Após isso, colocá-la no namespace Alura.Filmes.
 
 // 3.2 Criar um programa Program.cs, instanciar seus 5 filmes favoritos, guardá-los em uma lista e mostrar as suas informações no console.
+using Alura.Filmes;
+
+Filme filme1 = new Filme("Um sonho de liberdade", 142, new List<string>() {"Tim Robbins", "Morgan Freeman" });
+Filme filme2 = new Filme("O poderoso chefão", 175, new List<string>() { "Marlon Brando", "Al Pacino", "Talia Shire"});
+Filme filme3 = new Filme("Batman - O Cavaleiro das Trevas", 152, new List<string>() {"Christian Bale", "Heath Ledger", "Maggie Gyleenhaal" });
+Filme filme4 = new Filme("Senhor dos Anéis - O Retorno do Rei", 201, new List<string>() {"Elijah Wood", "Ian McKellen", "Viggo Mortensen" });
+Filme filme5 = new Filme("Green Book - O Guia", 130, new List<String>() { "Viggo Mortensen", "Mahershala Ali" });
+
+filme1.ExibirInformacoesDoFilme();
+filme2.ExibirInformacoesDoFilme();
+filme3.ExibirInformacoesDoFilme();
+filme4.ExibirInformacoesDoFilme();
+filme5.ExibirInformacoesDoFilme();
 
 // 3.3 Criar uma classe Artista, que representa uma pessoa que atua em filmes, no namespace Alura.Filmes. A classe deve conter atributos como o nome, idade e uma lista de filmes onde o artista atuou.
 
